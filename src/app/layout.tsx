1c838f3b46062e239e081d12d0fbbb905aba86ab
TheRealLorenz <lollobellina@gmail.com>
2023-12-20 18:53:44 +0100

    Setup styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "@/styles/global.scss";
+import "@/styles/variables.scss";
 
 export const metadata: Metadata = {
   title: "Bellonzo's Blog",
